4ce2e67fe128cb64d54f22e2ef0866e2598f2bf4
Bang Nguyen and Jim Klein <bang+jim@>
2016-08-18 10:25:44 -0700

    create comment-new form and clear-form functionality when clicking save button

--- app/components/comment-new.js
+++ app/components/comment-new.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save1() {
+      var params = {
+        name: this.get('name'),
+        date: this.get('date'),
+        body: this.get('body'),
+        post: this.get('post'),
+      };
+      this.set('name', "");
+      this.set('date', "");
+      this.set('body', "");
+      this.sendAction('save2', params);
+    }
+  }
 });
